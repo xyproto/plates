cdf1eb89af6c35fb6069ca334d550096a837ad53
Alexander F. Rødseth <rodseth@gmail.com>
2023-08-02 14:23:23 +0200

    Update the example program

--- README.md
+++ README.md
@@ -4,22 +4,86 @@ Package for dealing with RGB, HSV and HSL colors, reading and writing images.
 
 * [Online API documentation](http://godoc.org/github.com/xyproto/imagelib)
 
-
-Example
--------
+## Example
 
 ![puppy](cmd/puppyart/puppy.png)
 
-Transformed output
+The puppy image is from [Wikipedia](http://upload.wikimedia.org/wikipedia/commons/c/c7/Puppy_on_Halong_Bay.jpg).
+
+### Transformed output
 
 ![puppy](img/generated.png)
 
-The puppy image is from [Wikipedia](http://upload.wikimedia.org/wikipedia/commons/c/c7/Puppy_on_Halong_Bay.jpg).
+The above image was generated with the following example program:
 
+```go
+package main
 
-General information
--------------------
+import (
+    "image"
+    "image/color"
+    "image/draw"
+    "log"
+
+    "github.com/xyproto/imagelib"
+)
+
+// convert takes an image file and modifies it based on a specified threshold, intensity, and two colors.
+// It returns a processed image.
+func convert(infilename string, thresh uint8, t float64, color1, color2 color.RGBA) image.Image {
+    // Combine the two colors to create a mixed color
+    mixcolor := imagelib.PaintMix(color1, color2)
+
+    // Load the input image
+    img, err := imagelib.Read(infilename)
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+    // Separate the image into three based on the specified threshold and intensity: color1, mixcolor, and color2.
+    images := imagelib.Separate(img, color1, mixcolor, color2, thresh, t)
+    color1image, mixcolorimage, color2image := images[0], images[1], images[2]
+
+    // Combine the color1 image and mixcolor image to create a new image plate with color1
+    color1plate := imagelib.AddToAs(color1image, mixcolorimage, color1)
+
+    // Combine the color1plate and color2image to create an allplate image with color2 color
+    // And then combine the resulting image with the mixcolorimage to get the final output image.
+    allplate := imagelib.AddToAs(color1plate, color2image, color2)
+    allplate = imagelib.AddToAs(allplate, mixcolorimage, mixcolor)
+
+    return allplate
+}
+
+func main() {
+    // Define input colors and process the image using convert function.
+    // Repeat this process for different color combinations.
+    image1 := convert("puppy.png", 255, 1.0, color.RGBA{0, 0, 255, 255}, color.RGBA{255, 255, 255, 255})
+    image2 := convert("puppy.png", 255, 1.0, color.RGBA{16, 63, 255, 255}, color.RGBA{0, 0, 0, 255})
+    image3 := convert("puppy.png", 255, 1.0, color.RGBA{255, 0, 0, 255}, color.RGBA{255, 255, 0, 255})
+    image4 := convert("puppy.png", 255, 1.0, color.RGBA{16, 255, 255, 255}, color.RGBA{255, 0, 255, 255})
+
+    // Create a new image to contain the 4 processed images.
+    width := image1.Bounds().Dx() * 2
+    height := image2.Bounds().Dy() * 2
+    newimage := image.NewRGBA(image.Rect(0, 0, width, height))
+
+    // Arrange the 4 processed images on the new image.
+    draw.Draw(newimage, image1.Bounds(), image1, newimage.Bounds().Min, draw.Src)
+    draw.Draw(newimage, image.Rect(image1.Bounds().Dx(), 0, width, image1.Bounds().Dy()), image2, newimage.Bounds().Min, draw.Src)
+    draw.Draw(newimage, image.Rect(0, image1.Bounds().Dy(), image1.Bounds().Dx(), height), image3, newimage.Bounds().Min, draw.Src)
+    draw.Draw(newimage, image.Rect(image1.Bounds().Dx(), image1.Bounds().Dy(), width, height), image4, newimage.Bounds().Min, draw.Src)
+
+    // Write the final image to file.
+    if err := imagelib.Write("generated.png", newimage); err != nil {
+        log.Fatalln(err)
+    }
+}
+```
+
+## General information
 
 * License: BSD-3
 * Author: Alexander F. Rødseth &lt;xyproto@archlinux.org&gt;
+l
 * Version: 1.0.1
--- cmd/puppyart/main.go
+++ cmd/puppyart/main.go
@@ -1,83 +1,62 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"image/draw"
+	"log"
 
 	"github.com/xyproto/imagelib"
 )
 
-// TODO: refactor and comment
-
+// convert takes an image file and modifies it based on a specified threshold, intensity, and two colors.
+// It returns a processed image.
 func convert(infilename string, thresh uint8, t float64, color1, color2 color.RGBA) image.Image {
+	// Combine the two colors to create a mixed color
 	mixcolor := imagelib.PaintMix(color1, color2)
 
-	fmt.Println("(", mixcolor.R, mixcolor.G, mixcolor.B, ")")
-
-	// Input image
+	// Load the input image
 	img, err := imagelib.Read(infilename)
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 
-	// Separate the image to color1, mixcolor and color2, with given threshold
+	// Separate the image into three based on the specified threshold and intensity: color1, mixcolor, and color2.
 	images := imagelib.Separate(img, color1, mixcolor, color2, thresh, t)
 	color1image, mixcolorimage, color2image := images[0], images[1], images[2]
 
-	// Add the green image to the blue image with cyan color
-	// pseudo: blueplate = blueimage + greenimage in blue
+	// Combine the color1 image and mixcolor image to create a new image plate with color1
 	color1plate := imagelib.AddToAs(color1image, mixcolorimage, color1)
 
-	// Add the green image to the color2 image with color2 color
-	// pseudo: color2plate = color2image + greenimage in color2
-	//color2plate := imagelib.AddToAs(color2image, color2image, color2)
-	//color2plate = imagelib.AddToAs(color2image, mixcolorimage, color2)
-
-	// Add the color2 to the cyan and the green to the rest
+	// Combine the color1plate and color2image to create an allplate image with color2 color
+	// And then combine the resulting image with the mixcolorimage to get the final output image.
 	allplate := imagelib.AddToAs(color1plate, color2image, color2)
 	allplate = imagelib.AddToAs(allplate, mixcolorimage, mixcolor)
 
-	// Write out all the images
-	//imagelib.WritePNG(color1plate, color1filename)
-	//imagelib.WritePNG(color2plate, color2filename)
-	//imagelib.WritePNG(allplate, outfilename)
 	return allplate
 }
 
 func main() {
-	color1 := color.RGBA{0, 0, 255, 255}     // blue
-	color2 := color.RGBA{255, 255, 255, 255} // white
-	image1 := convert("puppy.png", 255, 1.0, color1, color2)
-	color1 = color.RGBA{16, 63, 255, 255} // greenblue
-	color2 = color.RGBA{0, 0, 0, 255}     // black
-	image2 := convert("puppy.png", 255, 1.0, color1, color2)
-	color1 = color.RGBA{255, 0, 0, 255}   // red
-	color2 = color.RGBA{255, 255, 0, 255} // yellow
-	image3 := convert("puppy.png", 255, 1.0, color1, color2)
-	color1 = color.RGBA{16, 255, 255, 255} // turqoise
-	color2 = color.RGBA{255, 0, 255, 255}  // purple
-	image4 := convert("puppy.png", 255, 1.0, color1, color2)
+	// Define input colors and process the image using convert function.
+	// Repeat this process for different color combinations.
+	image1 := convert("puppy.png", 255, 1.0, color.RGBA{0, 0, 255, 255}, color.RGBA{255, 255, 255, 255})
+	image2 := convert("puppy.png", 255, 1.0, color.RGBA{16, 63, 255, 255}, color.RGBA{0, 0, 0, 255})
+	image3 := convert("puppy.png", 255, 1.0, color.RGBA{255, 0, 0, 255}, color.RGBA{255, 255, 0, 255})
+	image4 := convert("puppy.png", 255, 1.0, color.RGBA{16, 255, 255, 255}, color.RGBA{255, 0, 255, 255})
 
+	// Create a new image to contain the 4 processed images.
 	width := image1.Bounds().Dx() * 2
 	height := image2.Bounds().Dy() * 2
-
 	newimage := image.NewRGBA(image.Rect(0, 0, width, height))
 
-	destrect := image1.Bounds()
-	zero := newimage.Bounds().Min
-	draw.Draw(newimage, destrect, image1, zero, draw.Src)
-	destrect = image.Rect(image1.Bounds().Dx(), 0, width, image1.Bounds().Dy())
-	draw.Draw(newimage, destrect, image2, zero, draw.Src)
-	destrect = image.Rect(0, image1.Bounds().Dy(), image1.Bounds().Dx(), height)
-	draw.Draw(newimage, destrect, image3, zero, draw.Src)
-	destrect = image.Rect(image1.Bounds().Dx(), image1.Bounds().Dy(), width, height)
-	fmt.Println(destrect)
-	draw.Draw(newimage, destrect, image4, zero, draw.Src)
+	// Arrange the 4 processed images on the new image.
+	draw.Draw(newimage, image1.Bounds(), image1, newimage.Bounds().Min, draw.Src)
+	draw.Draw(newimage, image.Rect(image1.Bounds().Dx(), 0, width, image1.Bounds().Dy()), image2, newimage.Bounds().Min, draw.Src)
+	draw.Draw(newimage, image.Rect(0, image1.Bounds().Dy(), image1.Bounds().Dx(), height), image3, newimage.Bounds().Min, draw.Src)
+	draw.Draw(newimage, image.Rect(image1.Bounds().Dx(), image1.Bounds().Dy(), width, height), image4, newimage.Bounds().Min, draw.Src)
 
-	err := imagelib.Write("generated.png", newimage)
-	if err != nil {
-		panic(err)
+	// Write the final image to file.
+	if err := imagelib.Write("generated.png", newimage); err != nil {
+		log.Fatalln(err)
 	}
 }
